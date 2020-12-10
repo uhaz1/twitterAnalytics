3fc645b1fff8495fd4f225896707b9eed58c6435
uhaz1 <uhaz1@yahoo.com.au>
2020-12-10 14:01:44 +0000

    Add files via upload

--- main.R
+++ main.R
@@ -193,3 +193,28 @@ ap_documents
 findAssocs(dtm, "benefit",0.2)
 findAssocs(dtm, "govern",0.1)
 
+
+library(LDAvis)
+
+topics <- posterior(ap_lda1)
+head(topics['topics'])
+head(topics['terms'])
+
+help(createJSON, package = "LDAvis")
+
+topicmodels2LDAvis <- function(x, ...){
+  post <- topicmodels::posterior(x)
+  if (ncol(post[["topics"]]) < 3) stop("The model must contain > 2 topics")
+  mat <- x@wordassignments
+  LDAvis::createJSON(
+    phi = post[["terms"]], 
+    theta = post[["topics"]],
+    vocab = colnames(post[["terms"]]),
+    doc.length = slam::row_sums(mat, na.rm = TRUE),
+    term.frequency = slam::col_sums(mat, na.rm = TRUE)
+  )
+}
+
+serVis(topicmodels2LDAvis(ap_lda1))
+
+
